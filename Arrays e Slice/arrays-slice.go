d10bcac9f75a295fd79e8fd9717108a423c22d9b
FerGBD <fergabriel1409@gmail.com>
2024-06-20 22:06:56 -0300

    Arrays e slices

--- Arrays e Slice/arrays-slice.go
+++ Arrays e Slice/arrays-slice.go
@@ -33,4 +33,22 @@ func main() {
 
 	array2[1] = "posição alterada"
 	fmt.Println(slice2)
+
+	//arrays internos
+	fmt.Println("-------------------------")
+	slice3 := make([]float32, 10, 11)
+
+	slice3 = append(slice3, 5)
+	slice3 = append(slice3, 6)
+
+	fmt.Println(slice3)
+	fmt.Println(len(slice3)) //length, tamanho
+	fmt.Println(cap(slice3)) //capacidade
+
+	slice4 := make([]float32, 5)
+	fmt.Println(slice4)
+	slice4 = append(slice4, 10)
+	fmt.Println(len(slice4))
+	fmt.Println(cap(slice4))
+
 }
