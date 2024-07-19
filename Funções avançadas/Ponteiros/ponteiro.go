8ba0dae3f70f635e0403a2c4ee3d6f4c83b5c36e
FerGBD <fergabriel1409@gmail.com>
2024-07-18 22:57:40 -0300

    Função com ponteiro

---
+++ Funções avançadas/Ponteiros/ponteiro.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func inverterSinal(numero int) int {
+	return numero * -1
+}
+
+func inverterSinalComPonteiro(numero *int) {
+	*numero = *numero * -1
+}
+
+func main() {
+	numero := -2
+	nuemroInvertido := inverterSinal(numero)
+	fmt.Println(nuemroInvertido)
+	fmt.Println(numero)
+
+	novoNumero := 40
+	fmt.Println(novoNumero)
+	inverterSinalComPonteiro(&novoNumero)
+	fmt.Println(novoNumero)
+
+}
